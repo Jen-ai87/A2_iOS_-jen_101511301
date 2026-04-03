d50157505e43a2d000d5e7c9d65bb1b7a4e9b865
Jennifer Henry <jen.henry@georgebrown.ca>
2026-04-03 16:34:14 -0400

    Added ProductView Tab to TabView

--- A2_iOS_ jen_101511301/ContentView.swift
+++ A2_iOS_ jen_101511301/ContentView.swift
@@ -3,7 +3,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-           
+            ProductBrowserView()
+                .tabItem {
+                    Label("Browse", systemImage: "house.fill")
+                }
+            
         }
     }
 }
