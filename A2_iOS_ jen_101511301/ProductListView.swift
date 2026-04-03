4f64dfbb6634502f4723fc73698ff89dd608b53b
Jennifer Henry <jen.henry@georgebrown.ca>
2026-04-03 17:48:46 -0400

    Added ProductDetailView to use inside ProductListView

--- A2_iOS_ jen_101511301/ProductListView.swift
+++ A2_iOS_ jen_101511301/ProductListView.swift
@@ -5,4 +5,24 @@ struct ProductListView: View {
     
 }
 
+struct ProductDetailView: View {
+    let product: Product
+
+    var body: some View {
+        List {
+            Section("Product Info") {
+                LabeledContent("ID", value: product.productID ?? "")
+                LabeledContent("Name", value: product.name ?? "")
+                LabeledContent("Provider", value: product.provider ?? "")
+                LabeledContent("Price", value: String(format: "$%.2f", product.price))
+            }
+            Section("Description") {
+                Text(product.productDescription ?? "")
+                    .font(.body)
+            }
+        }
+        .navigationTitle(product.name ?? "Product")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
 
