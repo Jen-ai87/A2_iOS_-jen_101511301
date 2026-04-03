1d02b246f04670b43301abb34b670561ce665f88
Jennifer Henry <jen.henry@georgebrown.ca>
2026-04-03 17:32:45 -0400

    Updated Entry Point of the App

--- A2_iOS_ jen_101511301/A2_iOS_jen_101511301App.swift
+++ A2_iOS_ jen_101511301/A2_iOS_jen_101511301App.swift
@@ -3,5 +3,16 @@ import CoreData
 
 @main
 struct A2_iOS_jen_101511301App: App {
-    
+    let persistenceController = PersistenceController.shared
+
+    init() {
+        DataSeeder.seedIfNeeded(context: PersistenceController.shared.container.viewContext)
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
 }
